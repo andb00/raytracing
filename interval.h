2b38aff504775b7d629e5da1b5f290cd6876b7fc
andrew <andrewbetancourt00@gmail.com>
2024-06-11 23:23:09 -0700

    Fixed interval logic

--- interval.h
+++ interval.h
@@ -9,12 +9,12 @@ class interval {
 public:
     double min, max;
 
-    interval() : min(+infinity), max(-infinity) {}
+    interval() : min(+infinity), max(-infinity) {} // Default interval is empty
     interval(double min, double max) : min(min), max(max) {}
 
     double size() const { return max - min; }
-    bool contains(double x) const { return min <= x && x <= max; }
-    bool surrounds(double x) const { return x <= min && x >= max; }
+    bool contains(double x) const { return min <= x && x <= max;}
+    bool surrounds(double x) const { return min < x && x < max; }
     double clamp(double x) const {
         if (x < min) return min;
         if (x > max) return max;
