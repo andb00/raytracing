45e30aee20b1d8f759dd38013bfcfac55ea583ee
andrew <andrewbetancourt00@gmail.com>
2024-06-12 20:26:29 -0700

    Limit amount of rays that bounce off surface

--- camera.h
+++ camera.h
@@ -12,9 +12,10 @@ using namespace std;
 
 class camera {
 public:
-    double aspect_ratio; // Ratio of width to height
-    int image_width; // Rendered image width in pixel count
-    int samples_per_pixel; // Count of random samples for each pixel
+    double aspect_ratio;    // Ratio of width to height
+    int image_width;        // Rendered image width in pixel count
+    int samples_per_pixel;  // Count of random samples for each pixel
+    int max_depth;          // Maximum number of bounces for a ray
 
     // Public Camera Parameters Here
 
@@ -32,7 +33,7 @@ public:
                 for (int sample = 0; sample < samples_per_pixel; sample++)
                 {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(cout, pixel_samples_scale * pixel_color);
             }
@@ -95,12 +96,16 @@ private:
         return {random_double() - 0.5, random_double() - 0.5, 0};
     }
 
-    static color ray_color(const ray& r, const hittable& world) {
+    static color ray_color(const ray& r, int depth, const hittable& world) {
+        // If we've exceeded the ray bounce limit, no more light is gathered
+        if (depth <= 0)
+            return color(0, 0, 0);
+
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
+        if (world.hit(r, interval(0.001, infinity), rec)) {
             vec3 direction = random_in_hemisphere(rec.normal);
-            return 0.5 * ray_color(ray(rec.p, direction), world);
+            return 0.5 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
