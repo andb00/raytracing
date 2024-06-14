b364782480cbfdaf0e3019aa1b5e2871702204c3
andrew <andrewbetancourt00@gmail.com>
2024-06-13 17:33:29 -0700

    reflective metal surfaces

--- vec3.h
+++ vec3.h
@@ -48,6 +48,12 @@ public:
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
 
+    bool near_zero() const {
+        // Return true if the vector is close to zero in all dimensions.
+        const auto s = 1e-8;
+        return (fabs(e[0]) < s) && (fabs(e[1]) < s) && (fabs(e[2]) < s);
+    }
+
     static vec3 random() {
         return vec3(random_double(), random_double(), random_double());
     }
@@ -116,9 +122,13 @@ inline vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
 }
 
-inline vec3 random_in_hemisphere(const vec3& normal) {
+inline vec3 random_on_hemisphere(const vec3& normal) {
     vec3 in_unit_sphere = random_in_unit_sphere();
     // In the same hemisphere as the normal
     return dot(in_unit_sphere, normal) > 0.0 ? in_unit_sphere : -in_unit_sphere;
+}
+
+inline vec3 reflect(const vec3& v, const vec3& n) {
+    return v - 2*dot(v,n)*n;
 }
 #endif //VEC3_H
